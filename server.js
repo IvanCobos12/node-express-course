881fb30b97491ca8cf584f7ef445a36318ca0804
SrCobos <86936544+IvanCobos12@users.noreply.github.com>
2021-07-27 10:48:19 -0600

    Update server.js
    add login POST route

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require ('express');
 const app = express();
 
+const  bodyParser  =  require ( 'body-parser' ) ; 
+app.use( bodyParser.json());
+
 
 const  mockUserData = [ 
 	{ nombre : 'Mark' } , 
@@ -17,11 +20,31 @@ app.get( '/users', function (req, res) {
 app.get( '/users:id', function (req, res) { 
 	res.json({ 
 		success : true, 
-		message : 'consiguió usuarios con éxito. ¡ Bien !', 
+		message : 'Obtuve un usuario heeee', 
 		users : req.params.id
 	}) 
 })
 
+app.post('/login',function(req,res){
+    const username=req.body.username;
+    const password=req.body.password;
+
+    const mockUsername="billyTheKid";
+    const mockPassword="superSecret";
+
+    if (username===mockUsername && password===mockPassword){
+        res.json({
+            success: true,
+            message: 'password and username match!',
+            token: 'encrypted token goes here'
+        })
+    } else {
+        res.json({
+            success: false,
+            message: 'password and username do not match'
+        })
+    }
+})
 
 app.listen( 8000 , function() { 
     console.log( "The server is running" ) ;
