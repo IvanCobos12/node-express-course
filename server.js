1e44581bd79b959ec9e10bd276d08326293643c7
SrCobos <86936544+IvanCobos12@users.noreply.github.com>
2021-07-27 10:15:14 -0600

    Update server.js
    add first GET route

--- server.js
+++ server.js
@@ -2,7 +2,18 @@ const express = require ('express');
 const app = express();
 
 
+const  mockUserData = [ 
+	{ nombre : 'Mark' } , 
+	{ nombre : 'Jill' } 
+]
 
+app.get( '/users', function ( req , res ) { 
+	res.json({ 
+		success : true, 
+		message : 'consiguió usuarios con éxito. ¡ Bien !', 
+		users : mockUserData
+	}) 
+} )
 
 
 
